0862cad1eb85a178244c5204a6740fbfe370e7c4
Bobur Bunyodjonov <80701998+BoburBunyodjonov@users.noreply.github.com>
2024-11-08 17:56:00 +0500

    back button bug fixed

--- src/components/elements/navbar/Navbar.tsx
+++ src/components/elements/navbar/Navbar.tsx
@@ -1,32 +1,35 @@
 import { Button, TextField } from "@mui/material";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import SearchIcon from "@mui/icons-material/Search";
 import useSearchContext from "./context/useSearch";
 import { ArrowBack } from "@mui/icons-material";
 
 const Navbar = () => {
+  const navigate = useNavigate();
   const location = useLocation();
   const showSearch =
-  location.pathname !== "/cart"
+    location.pathname !== "/cart"
 
   const {
-    state: {searchInput},
+    state: { searchInput },
     actions: { handleSearch, handleInputChange },
   } = useSearchContext();
+
+  const handleBackClick = () => {
+    navigate(-1);
+  };
+
   return (
     <>
       <div
         className="md:max-w-[500px] container mx-auto py-2 px-2 md:px-0 flex justify-between items-center sticky top-0 z-50 bg-white"
       >
-        <Link to="/" >
-          <Button variant="outlined" >
-          <ArrowBack  />
-          </Button>
-        </Link>
-
-          {
-            showSearch && (
-              <form className="flex items-center w-full justify-end space-x-3">
+        <Button variant="outlined" onClick={handleBackClick} >
+          <ArrowBack />
+        </Button>
+        {
+          showSearch && (
+            <form className="flex items-center w-full justify-end space-x-3">
               <TextField
                 size="small"
                 className="md:w-[80%] w-[60%]"
@@ -40,8 +43,8 @@ const Navbar = () => {
                 <SearchIcon />
               </Button>
             </form>
-            )
-          }
+          )
+        }
       </div>
     </>
   );
